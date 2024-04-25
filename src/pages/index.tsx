b963c0abb77c8476b34a5500a403bb3583e36ade
CaptainSkyFish <ajaystd9756@gmail.com>
2024-04-25 19:00:15 +0530

    nm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import { VideoCard } from "@/components/VideoCard";
 import { VideoGrid } from "@/components/VideoGrid";
 import { AppBar } from "@/components/AppBar";
 
